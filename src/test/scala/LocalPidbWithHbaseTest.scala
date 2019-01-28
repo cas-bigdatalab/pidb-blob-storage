7d9779e35e13cae282766a3c82dfb904a1f2e0bb
sha0w <coco11563@yeah.net>
2019-01-28 16:28:27 +0800

    revise the mvn pom file

--- src/test/scala/LocalPidbWithHbaseTest.scala
+++
@@ -1,9 +0,0 @@
-import java.io.File
-
-import cn.pidb.engine.PidbConnector
-
-class LocalPidbWithHbaseTest extends LocalPidbTest {
-  override def openDatabase() =
-    PidbConnector.openDatabase(new File("./testdb/data/databases/graph.db"),
-      new File("./neo4j-hbase.properties"));
-}
